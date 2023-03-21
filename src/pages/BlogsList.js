3eaae8beda56da5514ebb3be23d6ba34a2b9451d
Michael Fernandez <112970281+Michael-J-Fernandez@users.noreply.github.com>
2023-03-21 01:20:47 -0400

    Added Link to single blogs page.

--- src/pages/BlogsList.js
+++ src/pages/BlogsList.js
@@ -1,15 +1,19 @@
-
+import { Link } from 'react-router-dom';
 
 const BlogsList = ({ blogs }) => {
     
     return ( 
         <div className="blogs">
             {blogs && blogs.map(blog => (
-                <div className="blog-card">
-                <h1>{blog.title}</h1>
-                <h3>By {blog.author}</h3>
-                <p>{blog.text.substring(1, 70)}...</p>
-                </div>
+                <Link to={`${blog.id}`}>
+                    <div
+                        className="blog-card"
+                    >
+                    <h1>{blog.title}</h1>
+                    <h3>By {blog.author}</h3>
+                    <p>{blog.text.substring(1, 70)}...</p>
+                    </div>
+                </Link>
             ))}
         </div>
     );
