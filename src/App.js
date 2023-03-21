63ab9112c62b911bb7bb075790a15d6259bdd71c
Michael Fernandez <112970281+Michael-J-Fernandez@users.noreply.github.com>
2023-03-20 20:07:42 -0400

    Added base routes for /home, /blogs.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+
+import NavLayout from './layouts/NavLayout';
+import Home from './pages/Home';
+import BlogsList from './pages/BlogsList'
+
+import blogs from './api/api'
 
 function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={ <NavLayout /> } >
+        <Route index element={ <Home /> } />
+        <Route path="blogs" element={ <BlogsList blogs={blogs} /> } />
+      </Route>
+    )
+  )
+
   return (
     <div className="App">
-
+      <RouterProvider router={router} />
     </div>
   );
 }
