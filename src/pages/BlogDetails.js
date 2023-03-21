79c9fc479e5fca6dad0450f41850ddbb41172976
Michael Fernandez <112970281+Michael-J-Fernandez@users.noreply.github.com>
2023-03-21 16:21:55 -0400

    Added deleteBlog functionality. Deletes blog from db.

--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -1,16 +1,39 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import api from '../api/blogs'
 
-const BlogDetails = ({ blogs }) => {
+const BlogDetails = ({ blogs, setBlogs }) => {
 
     const { id } = useParams();
+    const navigate = useNavigate();
 
     const singleBlog = blogs.find(blog => blog.id === id)
+
+    const deleteBlog = async () => {
+      try {
+        const response = await api.delete(`${id}`);
+        
+        const filteredBlogs = blogs.filter(blog => blog.id !== id)
+
+        setBlogs([ ...filteredBlogs ]);
+        navigate('/blogs')
+
+      } catch (error) {
+        console.log(error.message);
+      }
+      console.log('Will delete blog with id: ' + id)
+    }
 
     return (
       <>
         <Link className={"back-link"} to=".." relative="path">
           ⬅️ Back to Blogs
         </Link>
+        <div
+          className="delete"
+          onClick={deleteBlog}
+        >
+          [ X ]
+        </div>
         {singleBlog && (
           <>
             <h1>{singleBlog.title}</h1>
