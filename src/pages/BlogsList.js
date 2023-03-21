f403ea3515ba436fbc25c3bfaad17447eedbb388
Michael Fernandez <112970281+Michael-J-Fernandez@users.noreply.github.com>
2023-03-21 12:46:25 -0400

    Added conditional rendering of blog list to prevent app from crashing. Moved Link inside the blog card for better style formatting.

--- src/pages/BlogsList.js
+++ src/pages/BlogsList.js
@@ -4,17 +4,20 @@ const BlogsList = ({ blogs }) => {
     
     return ( 
         <div className="blogs">
-            {blogs && blogs.map(blog => (
-                <Link to={`${blog.id}`}>
+            {blogs.length > 0 ?
+                blogs.map(blog => (
                     <div
                         className="blog-card"
-                    >
-                    <h1>{blog.title}</h1>
-                    <h3>By {blog.author}</h3>
-                    <p>{blog.text.substring(1, 70)}...</p>
+                        key={blog.id}
+                        >
+                        <Link to={`${blog.id}`}>
+                        <h1>{blog.title}</h1>
+                        <h3>By {blog.author}</h3>
+                        <p>{blog.text.substring(1, 70)}...</p>
+                        </Link>
                     </div>
-                </Link>
-            ))}
+                ))
+                : <h1>Loading...</h1>}
         </div>
     );
 }
