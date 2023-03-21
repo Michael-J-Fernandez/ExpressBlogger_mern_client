e88ef3082b8d9d124857726de57c4a00a61aaa36
Michael Fernandez <112970281+Michael-J-Fernandez@users.noreply.github.com>
2023-03-21 14:27:24 -0400

    Cleaned up formatting for better readability.

--- src/pages/BlogsList.js
+++ src/pages/BlogsList.js
@@ -1,25 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const BlogsList = ({ blogs }) => {
-    
-    return ( 
-        <div className="blogs">
-            {blogs.length > 0 ?
-                blogs.map(blog => (
-                    <div
-                        className="blog-card"
-                        key={blog.id}
-                        >
-                        <Link to={`${blog.id}`}>
-                        <h1>{blog.title}</h1>
-                        <h3>By {blog.author}</h3>
-                        <p>{blog.text.substring(1, 70)}...</p>
-                        </Link>
-                    </div>
-                ))
-                : <h1>Loading...</h1>}
-        </div>
-    );
-}
- 
+  return (
+    <div className="blogs">
+      {blogs.length > 0 ? (
+        blogs.map(blog => (
+          <div className="blog-card" key={blog.id}>
+            <Link to={`${blog.id}`}>
+              <h1>{blog.title}</h1>
+              <h3>By {blog.author}</h3>
+              <p>{blog.text.substring(1, 70)}...</p>
+            </Link>
+          </div>
+        ))
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </div>
+  );
+};
+
 export default BlogsList;
