7a4309db864c496a1767525f0da8308deca8ccc8
Michael Fernandez <112970281+Michael-J-Fernandez@users.noreply.github.com>
2023-03-21 00:45:25 -0400

    Added route for NewBlog page. Created blogs state and passed as props to NewBlog.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import { useState } from 'react';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route
+} from 'react-router-dom';
 
 import NavLayout from './layouts/NavLayout';
 import Home from './pages/Home';
 import BlogsList from './pages/BlogsList'
+import NewBlog from './pages/NewBlog';
 
-import blogs from './api/api'
+import blogsData from './api/api'
+import { useEffect } from 'react';
 
 function App() {
+
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    setBlogs(blogsData)
+  }, [])
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route element={ <NavLayout /> } >
         <Route index element={ <Home /> } />
         <Route path="blogs" element={ <BlogsList blogs={blogs} /> } />
+        <Route path="new-blog" element={ <NewBlog blogs={blogs} setBlogs={setBlogs} /> } />
       </Route>
     )
   )
