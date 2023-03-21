679d25fa5a80b8925e16ffdfdacc33c87d920394
Michael Fernandez <112970281+Michael-J-Fernandez@users.noreply.github.com>
2023-03-21 00:41:25 -0400

    Added NavLink for New Blog page.

--- src/layouts/NavLayout.js
+++ src/layouts/NavLayout.js
@@ -1,14 +1,19 @@
-import { NavLink, Outlet } from 'react-router-dom'
+import { NavLink, Link, Outlet } from 'react-router-dom'
 
 const NavLayout = () => {
     return (
         <>
         <nav className="nav-main">
-            <NavLink to="/"><h1>Home</h1></NavLink>
-            <NavLink to="blogs"><h1>Blogs</h1></NavLink>
-            <NavLink to="new-blog"><h1>New Blog</h1></NavLink>
+            <Link to="/"><h1 className="home-title">MERN Blogger</h1></Link>
+            <div className="nav-links">
+                <NavLink to="/"><h1>Home</h1></NavLink>
+                <NavLink to="blogs"><h1>Blogs</h1></NavLink>
+                <NavLink to="new-blog"><h1>+ New</h1></NavLink>
+            </div>
         </nav>
-        <Outlet />
+        <main className="main-content">
+            <Outlet />
+        </main>
         </>
     );
 }
