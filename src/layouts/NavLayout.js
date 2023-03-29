409d028afd904eacd7c5bcf00133ad25fe96a2f8
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-03-29 15:35:31 -0400

    Add active styles to NavLinks.

--- src/layouts/NavLayout.js
+++ src/layouts/NavLayout.js
@@ -1,14 +1,20 @@
 import { NavLink, Link, Outlet } from 'react-router-dom'
 
 const NavLayout = () => {
+
+    const active = {
+        color: "black",
+        textDecoration: "underline"
+    }
+
     return (
         <>
         <nav className="nav-main">
             <Link to="/"><h1 className="home-title">MERN Blogger</h1></Link>
             <div className="nav-links">
-                <NavLink to="/"><h1>Home</h1></NavLink>
-                <NavLink to="blogs"><h1>Blogs</h1></NavLink>
-                <NavLink to="new-blog"><h1>+ New</h1></NavLink>
+                <NavLink to="/" style={({isActive}) => isActive ? active : null}><h1>Home</h1></NavLink>
+                <NavLink to="blogs" style={({isActive}) => isActive ? active : null}><h1>Blogs</h1></NavLink>
+                <NavLink to="new-blog" style={({isActive}) => isActive ? active : null}><h1>+ New</h1></NavLink>
             </div>
         </nav>
         <main className="main-content">
