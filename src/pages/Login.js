fb92ada8f55d256dfbe8d4dd6978771bda9741a3
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-03-31 23:00:10 -0400

    Create login component and form.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,51 @@
+import { useState } from "react";
+
 const Login = () => {
-    return (
-        <h1>This is my LOGIN page!</h1>
-    );
-}
- 
+  const resetInput = {
+    email: "",
+    password: "",
+  };
+
+  const [loginInput, setLoginInput] = useState(resetInput);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setLoginInput((prev) => ({ ...prev, [name]: value }));
+    console.log(loginInput);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(loginInput);
+  };
+
+  return (
+    <form className="login-form" onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        {" "}
+        Email:
+        <input
+          type="email"
+          name="email"
+          id="email"
+          onChange={(e) => handleChange(e)}
+          value={loginInput.email}
+        />
+      </label>
+      <label htmlFor="password">
+        {" "}
+        Password:
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={(e) => handleChange(e)}
+          value={loginInput.password}
+        />
+      </label>
+    </form>
+  );
+};
+
 export default Login;
