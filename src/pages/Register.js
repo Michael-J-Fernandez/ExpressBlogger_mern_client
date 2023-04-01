901a8404f0173a08b2bfbc9378d87998b4df7c2c
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-04-01 01:17:53 -0400

    Create register form.

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,50 @@
+import { useState } from "react";
+
 const Register = () => {
-    return (
-        <h1>This is my REGISTER page!</h1>
-    );
+  const resetInput = {
+    email: "",
+    password: "",
+  };
+
+  const [registerInput, setRegisterInput] = useState(resetInput);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setRegisterInput((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setRegisterInput(resetInput);
+  };
+
+  return (
+    <form className="register-form" onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email:
+        <input
+          type="email"
+          name="email"
+          id="email"
+          onChange={(e) => handleChange(e)}
+          value={registerInput.email}
+        />
+      </label>
+      <label htmlFor="password">
+        Password:
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={(e) => handleChange(e)}
+          value={registerInput.password}
+        />
+      </label>
+      <button type="submit">Register</button>
+    </form>
+  );
 }
  
 export default Register;
