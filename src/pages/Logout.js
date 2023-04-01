32279042cd1214df366d9f3d5a99957587ec148a
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-04-01 03:41:16 -0400

    Added /logout api get request to clear token from cookies.

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,6 +1,18 @@
+import { useEffect } from "react"
+import { Navigate } from "react-router-dom";
+import api from "../api/blogs"
+
 const Logout = () => {
+
+    useEffect(() => {
+        const logOut = async () => {
+            await api.get("/users/logout");
+        }
+        logOut();
+    })
+
     return (
-        <h1>This is my LOGOUT page!</h1>
+        <Navigate to="/" />
     );
 }
  
