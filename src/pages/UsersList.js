ea0b27c0367032d554f341306d23c85f61222f71
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-04-04 15:50:46 -0400

    Added code to search users by query terms and user role.

--- src/pages/UsersList.js
+++ src/pages/UsersList.js
@@ -3,19 +3,82 @@ import api from "../api/axios";
 
 const UsersList = () => {
   const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState({ query: "", role: "all"});
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const usersData = await api.get("/users");
-      setUsers(usersData.data.users);
+      const { data } = await api.get("/users");
+      setUsers(data.users);
     };
     fetchUsers();
   }, []);
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFilter((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const filteredUsers = users.filter(user => {
+    if (filter.role === "all") {
+      return true;
+    } else {
+      return user.userRole === filter.role
+    }
+    
+  }).filter(user => user.email.includes(filter.query));
+  
+
   return (
     <div className="users-list">
+      <div className="users-filter">
+        <label htmlFor="query">
+          <input
+            type="text"
+            name="query"
+            id="query"
+            autoComplete="false"
+            placeholder="Search..."
+            value={filter.query}
+            onChange={(e) => handleChange(e)}
+          />
+        </label>
+        <label htmlFor="all">
+          <input
+            type="radio"
+            name="role"
+            id="all"
+            value="all"
+            checked={filter.role === "all"}
+            onChange={(e) => handleChange(e)}
+          />
+           All
+        </label>
+        <label htmlFor="user">
+          <input
+            type="radio"
+            name="role"
+            id="user"
+            value="user"
+            checked={filter.role === "user"}
+            onChange={(e) => handleChange(e)}
+          />
+           User
+        </label>
+        <label htmlFor="admin">
+          <input
+            type="radio"
+            name="role"
+            id="admin"
+            value="admin"
+            checked={filter.role === "admin"}
+            onChange={(e) => handleChange(e)}
+          />
+           Admin
+        </label>
+      </div>
       {users ? (
-        users.map((user) => <p key={user.id}>{user.email}</p>)
+        filteredUsers.map((user) => <p key={user.id}>{user.email}</p>)
       ) : (
         <h1>Loading...</h1>
       )}
