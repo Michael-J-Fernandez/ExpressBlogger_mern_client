8e2d3167dcc80caca4e393e066a13bc2f34b4bbd
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-04-04 14:16:59 -0400

    Added welcome message for logged in user, conditionally rendered.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
+
+
 const Home = () => {
-    return ( 
-        <h1>Welcome to Blogs Home!</h1>
-    );
+
+    let user = null;
+
+    if (process.env.REACT_APP_TOKEN_HEADER_KEY in localStorage) {
+        
+        const userData = localStorage.getItem(process.env.REACT_APP_TOKEN_HEADER_KEY);
+
+        user = JSON.parse(userData).user;
+    }
+    
+    const welcomeMessage = user ? `, ${user}` : null;
+        
+    return <h1>Welcome to Blogs Home{welcomeMessage}!</h1>;
 }
  
 export default Home;
