b2a1f917ed1ca40f3061a106a375dffb6720ed76
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-04-04 23:30:51 -0400

    Changed displayed name on welcome message from user's email to user's name.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,17 @@
-
-
 const Home = () => {
+  let name = null;
 
-    let user = null;
+  if (process.env.REACT_APP_TOKEN_HEADER_KEY in localStorage) {
+    const userData = localStorage.getItem(
+      process.env.REACT_APP_TOKEN_HEADER_KEY
+    );
 
-    if (process.env.REACT_APP_TOKEN_HEADER_KEY in localStorage) {
-        
-        const userData = localStorage.getItem(process.env.REACT_APP_TOKEN_HEADER_KEY);
+    name = JSON.parse(userData).name;
+  }
 
-        user = JSON.parse(userData).user;
-    }
-    
-    const welcomeMessage = user ? `, ${user}` : null;
-        
-    return <h1>Welcome to Blogs Home{welcomeMessage}!</h1>;
-}
- 
+  const welcomeMessage = name ? `, ${name}` : null;
+
+  return <h1>Welcome to Blogs Home{welcomeMessage}!</h1>;
+};
+
 export default Home;
